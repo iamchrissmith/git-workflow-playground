b8125a5778a9f6744c4bc3590c10066386def84f
Chris Smith <42@chrisryansmith.com>
2017-05-14 12:31:51 -0600

    add 'notes' files for reference during presentation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,14 @@
 class ArticlesController < ApplicationController
+
+
+
+
+
+
+  # def show
+  #   @article = Article.find(params[:id])
+  # end
+  # def new
+  #   @article = Article.new
+  # end
 end
