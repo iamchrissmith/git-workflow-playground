1cb415325309076c403f6f6073f0ea11fcb025ba
Chris Smith <42@chrisryansmith.com>
2017-05-14 12:36:50 -0600

    move class notes to separate dir

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,2 @@
 class ArticlesController < ApplicationController
-
-
-
-
-
-
-  # def show
-  #   @article = Article.find(params[:id])
-  # end
-  # def new
-  #   @article = Article.new
-  # end
 end
